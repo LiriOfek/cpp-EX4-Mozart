3060fcac0c1f2c446bcac35427940f7eb7c1be2a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-18 18:54:13 +0300

    add main function with documentation

---
+++ Ex5-Mozart/main.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (18/4/2020)
+Purpose: This contain functions that generate random vector,
+		 and function that convert vector to string,
+		 and function that make composition of them
+\********************************************************/
